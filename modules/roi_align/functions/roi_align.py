ea16d8f58433fcf1c594f2859c18225946e0cd44
songheony <songheony@gmail.com>
2020-12-02 22:05:36 +0900

    fix bug

--- modules/roi_align/functions/roi_align.py
+++ modules/roi_align/functions/roi_align.py
@@ -1,139 +1,117 @@
-import torch
 from torch.autograd import Function
-from .._ext2 import roi_align
+
+from .. import roi_align_cuda
 
 
-# TODO use save_for_backward instead
 class RoIAlignFunction(Function):
+
     @staticmethod
-    def forward(ctx, features, rois, aligned_height, aligned_width, spatial_scale):
-        aligned_width = int(aligned_width)
-        aligned_height = int(aligned_height)
-        spatial_scale = float(spatial_scale)
-        feature_size = features.size()
+    def forward(ctx, features, rois, out_size, spatial_scale, sample_num=0):
+        if isinstance(out_size, int):
+            out_h = out_size
+            out_w = out_size
+        elif isinstance(out_size, tuple):
+            
+            assert len(out_size) == 2
+            assert isinstance(out_size[0], int)
+            assert isinstance(out_size[1], int)
+            out_h, out_w = out_size
+        else:
+            raise TypeError(
+                '"out_size" must be an integer or tuple of integers')
+        ctx.spatial_scale = spatial_scale
+        ctx.sample_num = sample_num
+        ctx.save_for_backward(rois)
+        ctx.feature_size = features.size()
 
         batch_size, num_channels, data_height, data_width = features.size()
         num_rois = rois.size(0)
 
-        output = features.new(num_rois, num_channels, aligned_height, aligned_width).zero_()
-        #help(roi_align.roi_align_forward_cuda)
-        #print(features.type())
-        #print(rois.type())
-        #print(output.type())
+        output = features.new_zeros(num_rois, num_channels, out_h, out_w)
         if features.is_cuda:
-            success = roi_align.roi_align_forward_cuda(aligned_height,
-                                             aligned_width,
-                                             spatial_scale, features,
-                                             rois, output)
+            features = features.contiguous()
+            roi_align_cuda.forward(features, rois, out_h, out_w, spatial_scale,
+                                   sample_num, output)
         else:
             raise NotImplementedError
-
-        ctx.save_for_backward(rois, aligned_height, aligned_width, spatial_scale, feature_size)
 
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        rois, aligned_height, aligned_width, spatial_scale, feature_size = ctx.saved_tensors
-        assert(feature_size is not None and grad_output.is_cuda)
+        feature_size = ctx.feature_size
+        spatial_scale = ctx.spatial_scale
+        sample_num = ctx.sample_num
+        rois = ctx.saved_tensors[0]
+        assert (feature_size is not None and grad_output.is_cuda)
 
         batch_size, num_channels, data_height, data_width = feature_size
+        out_w = grad_output.size(3)
+        out_h = grad_output.size(2)
 
-        grad_input = rois.new(batch_size, num_channels, data_height,
-                                  data_width).zero_()
-        roi_align.roi_align_backward_cuda(aligned_height,
-                                          aligned_width,
-                                          spatial_scale, grad_output,
-                                          rois, grad_input)
+        grad_input = grad_rois = None
+        if ctx.needs_input_grad[0]:
+            grad_input = rois.new_zeros(batch_size, num_channels, data_height,
+                                        data_width)
+            roi_align_cuda.backward(grad_output.contiguous(), rois, out_h,
+                                    out_w, spatial_scale, sample_num,
+                                    grad_input)
 
-        # print grad_input
-
-        return grad_input, None
-
-
-# TODO use save_for_backward instead
+        return grad_input, grad_rois, None, None, None
 class RoIAlignAdaFunction(Function):
+
     @staticmethod
-    def forward(ctx, features, rois, aligned_height, aligned_width, spatial_scale):
-        aligned_width = int(aligned_width)
-        aligned_height = int(aligned_height)
-        spatial_scale = float(spatial_scale)
-        feature_size = features.size()
+    def forward(ctx, features, rois, out_size, spatial_scale, sample_num=0):
+        if isinstance(out_size, int):
+            out_h = out_size
+            out_w = out_size
+        elif isinstance(out_size, tuple):
+            assert len(out_size) == 2
+            assert isinstance(out_size[0], int)
+            assert isinstance(out_size[1], int)
+            out_h, out_w = out_size
+        else:
+            raise TypeError(
+                '"out_size" must be an integer or tuple of integers')
+        ctx.spatial_scale = spatial_scale
+        ctx.sample_num = sample_num
+        ctx.save_for_backward(rois)
+        ctx.feature_size = features.size()
 
         batch_size, num_channels, data_height, data_width = features.size()
         num_rois = rois.size(0)
 
-        output = features.new(num_rois, num_channels, aligned_height, aligned_width).zero_()
+        output = features.new_zeros(num_rois, num_channels, out_h, out_w)
         if features.is_cuda:
-            success = roi_align.roi_align_ada_forward_cuda(aligned_height,
-                                             aligned_width,
-                                             spatial_scale, features,
-                                             rois, output)
+            features = features.contiguous()
+            roi_align_cuda.ada_forward(features, rois, out_h, out_w, spatial_scale,
+                                   sample_num, output)
         else:
             raise NotImplementedError
-
-        ctx.save_for_backward(rois, aligned_height, aligned_width, spatial_scale, feature_size)
 
         return output
 
     @staticmethod
     def backward(ctx, grad_output):
-        rois, aligned_height, aligned_width, spatial_scale, feature_size = ctx.saved_tensors
-        assert(feature_size is not None and grad_output.is_cuda)
+        feature_size = ctx.feature_size
+        spatial_scale = ctx.spatial_scale
+        sample_num = ctx.sample_num
+        rois = ctx.saved_tensors[0]
+        assert (feature_size is not None and grad_output.is_cuda)
 
         batch_size, num_channels, data_height, data_width = feature_size
+        out_w = grad_output.size(3)
+        out_h = grad_output.size(2)
 
-        grad_input = rois.new(batch_size, num_channels, data_height,
-                                  data_width).zero_()
-        roi_align.roi_align_ada_backward_cuda(aligned_height,
-                                          aligned_width,
-                                          spatial_scale, grad_output,
-                                          rois, grad_input)
+        grad_input = grad_rois = None
+        if ctx.needs_input_grad[0]:
+            grad_input = rois.new_zeros(batch_size, num_channels, data_height,
+                                        data_width)
+            roi_align_cuda.ada_backward(grad_output.contiguous(), rois, out_h,
+                                    out_w, spatial_scale, sample_num,
+                                    grad_input)
 
-        # print grad_input
+        return grad_input, grad_rois, None, None, None
 
-        return grad_input, None
-
-
-# TODO use save_for_backward instead
-class RoIAlignDenseAdaFunction(Function):
-    @staticmethod
-    def forward(ctx, features, rois, aligned_height, aligned_width, spatial_scale):
-        aligned_width = int(aligned_width)
-        aligned_height = int(aligned_height)
-        spatial_scale = float(spatial_scale)
-        feature_size = features.size()
-
-        batch_size, num_channels, data_height, data_width = features.size()
-        num_rois = rois.size(0)
-
-        output = features.new(num_rois, num_channels, aligned_height, aligned_width).zero_()
-        if features.is_cuda:
-            success = roi_align.roi_align_dense_ada_forward_cuda(aligned_height,
-                                             aligned_width,
-                                             spatial_scale, features,
-                                             rois, output)
-        else:
-            raise NotImplementedError
-
-        ctx.save_for_backward(rois, aligned_height, aligned_width, spatial_scale, feature_size)
-
-        return output
-
-    @staticmethod
-    def backward(self, grad_output):
-        rois, aligned_height, aligned_width, spatial_scale, feature_size = ctx.saved_tensors
-        assert(feature_size is not None and grad_output.is_cuda)
-
-        batch_size, num_channels, data_height, data_width = feature_size
-
-        grad_input = rois.new(batch_size, num_channels, data_height,
-                                  data_width).zero_()
-        roi_align.roi_align_dense_ada_backward_cuda(aligned_height,
-                                          aligned_width,
-                                          spatial_scale, grad_output,
-                                          rois, grad_input)
-
-        # print grad_input
-
-        return grad_input, None
+roi_align = RoIAlignFunction.apply
+roi_align_ada = RoIAlignAdaFunction.apply
--- modules/roi_align/src/roi_align_cuda.cpp
+++ modules/roi_align/src/roi_align_cuda.cpp
@@ -1,258 +1,156 @@
-//#include <THC/THC.h>
-//#include <ATen/ATen.h>
-#include <ATen/cuda/CUDAContext.h>
-#include "roi_align_cuda.hpp"
-#include <math.h>
-#include "cuda/roi_align_kernel.h"
-//#include <torch/extension.h>
-extern THCState *state;
+#include <torch/extension.h>
 
-extern "C" int roi_align_forward_cuda(int aligned_height, int aligned_width, float spatial_scale,
-                        at::Tensor& features, at::Tensor& rois, at::Tensor& output)
-{
-    // Number of ROIs
-    int num_rois = rois.size(0);
-    int size_rois = rois.size(1);
-    if (size_rois != 5)
-    {
-        return 0;
-    }
-    // batch size
-    //int batch_size = THCudaTensor_size(state, features, 0);
-    //if (batch_size != 1)
-    //{
-    //    return 0;
-    //}
+#include <cmath>
+#include <vector>
 
+int ROIAlignForwardLaucher(const at::Tensor features, const at::Tensor rois,
+                           const float spatial_scale, const int sample_num,
+                           const int channels, const int height,
+                           const int width, const int num_rois,
+                           const int pooled_height, const int pooled_width,
+                           at::Tensor output);
 
-    // data height
-    int data_height = features.size(2);
-    // data width
-    int data_width = features.size(3);
-    // Number of channels
-    int num_channels = features.size(1);
+int ROIAlignBackwardLaucher(const at::Tensor top_grad, const at::Tensor rois,
+                            const float spatial_scale, const int sample_num,
+                            const int channels, const int height,
+                            const int width, const int num_rois,
+                            const int pooled_height, const int pooled_width,
+                            at::Tensor bottom_grad);
 
-    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+int ROIAlignAdaForwardLaucher(const at::Tensor features, const at::Tensor rois,
+                           const float spatial_scale, const int sample_num,
+                           const int channels, const int height,
+                           const int width, const int num_rois,
+                           const int pooled_height, const int pooled_width,
+                           at::Tensor output);
 
-    ROIAlignForwardLaucher(
-        features.contiguous().data<float>(), spatial_scale, num_rois, data_height,
-        data_width, num_channels, aligned_height,
-        aligned_width, rois.contiguous().data<float>(),
-        output.contiguous().data<float>(), stream);
+int ROIAlignAdaBackwardLaucher(const at::Tensor top_grad, const at::Tensor rois,
+                            const float spatial_scale, const int sample_num,
+                            const int channels, const int height,
+                            const int width, const int num_rois,
+                            const int pooled_height, const int pooled_width,
+                            at::Tensor bottom_grad);
 
-    return 1;
+#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x, " must be a CUDAtensor ")
+#define CHECK_CONTIGUOUS(x) \
+  TORCH_CHECK(x.is_contiguous(), #x, " must be contiguous ")
+#define CHECK_INPUT(x) \
+  CHECK_CUDA(x);       \
+  CHECK_CONTIGUOUS(x)
+
+int roi_align_forward_cuda(at::Tensor features, at::Tensor rois,
+                           int pooled_height, int pooled_width,
+                           float spatial_scale, int sample_num,
+                           at::Tensor output) {
+  CHECK_INPUT(features);
+  CHECK_INPUT(rois);
+  CHECK_INPUT(output);
+
+  // Number of ROIs
+  int num_rois = rois.size(0);
+  int size_rois = rois.size(1);
+
+  if (size_rois != 5) {
+    printf("wrong roi size\n");
+    return 0;
+  }
+
+  int num_channels = features.size(1);
+  int data_height = features.size(2);
+  int data_width = features.size(3);
+
+  ROIAlignForwardLaucher(features, rois, spatial_scale, sample_num,
+                         num_channels, data_height, data_width, num_rois,
+                         pooled_height, pooled_width, output);
+
+  return 1;
 }
 
-extern "C" int roi_align_backward_cuda(int aligned_height, int aligned_width, float spatial_scale,
-                        at::Tensor& top_grad, at::Tensor& rois, at::Tensor& bottom_grad)
-{
+int roi_align_backward_cuda(at::Tensor top_grad, at::Tensor rois,
+                            int pooled_height, int pooled_width,
+                            float spatial_scale, int sample_num,
+                            at::Tensor bottom_grad) {
+  CHECK_INPUT(top_grad);
+  CHECK_INPUT(rois);
+  CHECK_INPUT(bottom_grad);
 
-    // Number of ROIs
-    int num_rois = rois.size(0);
-    int size_rois = rois.size(1);
-    if (size_rois != 5)
-    {
-        return 0;
-    }
+  // Number of ROIs
+  int num_rois = rois.size(0);
+  int size_rois = rois.size(1);
+  if (size_rois != 5) {
+    printf("wrong roi size\n");
+    return 0;
+  }
 
-    // batch size
-    int batch_size = bottom_grad.size(0);
-    //if (batch_size != 1)
-    //{
-     //   return 0;
-    //}
-    // data height
-    int data_height = bottom_grad.size(2);
-    // data width
-    int data_width = bottom_grad.size(3);
-    // Number of channels
-    int num_channels = bottom_grad.size(1);
+  int num_channels = bottom_grad.size(1);
+  int data_height = bottom_grad.size(2);
+  int data_width = bottom_grad.size(3);
 
-    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-    ROIAlignBackwardLaucher(
-        top_grad.contiguous().data<float>(), spatial_scale, batch_size, num_rois, data_height,
-        data_width, num_channels, aligned_height,
-        aligned_width, rois.contiguous().data<float>(),
-        bottom_grad.contiguous().data<float>(), stream);
+  ROIAlignBackwardLaucher(top_grad, rois, spatial_scale, sample_num,
+                          num_channels, data_height, data_width, num_rois,
+                          pooled_height, pooled_width, bottom_grad);
 
-    return 1;
+  return 1;
 }
 
+int roi_align_ada_forward_cuda(at::Tensor features, at::Tensor rois,
+                           int pooled_height, int pooled_width,
+                           float spatial_scale, int sample_num,
+                           at::Tensor output) {
+  CHECK_INPUT(features);
+  CHECK_INPUT(rois);
+  CHECK_INPUT(output);
 
-extern "C" int roi_align_ada_forward_cuda(int aligned_height, int aligned_width, float spatial_scale,
-                        at::Tensor& features, at::Tensor& rois, at::Tensor& output)
-{
-    // Number of ROIs
-    int num_rois = rois.size(0);
-    int size_rois = rois.size(1);
-    if (size_rois != 5)
-    {
-        return 0;
-    }
-    // batch size
-    //int batch_size = THCudaTensor_size(state, features, 0);
-    //if (batch_size != 1)
-    //{
-    //    return 0;
-    //}
+  // Number of ROIs
+  int num_rois = rois.size(0);
+  int size_rois = rois.size(1);
 
+  if (size_rois != 5) {
+    printf("wrong roi size\n");
+    return 0;
+  }
 
-    // data height
-    int data_height = features.size(2);
-    // data width
-    int data_width = features.size(3);
-    // Number of channels
-    int num_channels = features.size(1);
+  int num_channels = features.size(1);
+  int data_height = features.size(2);
+  int data_width = features.size(3);
 
-    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
+  ROIAlignAdaForwardLaucher(features, rois, spatial_scale, sample_num,
+                         num_channels, data_height, data_width, num_rois,
+                         pooled_height, pooled_width, output);
 
-    ROIAlignAdaForwardLaucher(
-        features.contiguous().data<float>(), spatial_scale, num_rois, data_height,
-        data_width, num_channels, aligned_height,
-        aligned_width, rois.contiguous().data<float>(),
-        output.contiguous().data<float>(), stream);
-
-    return 1;
+  return 1;
 }
 
-extern "C" int roi_align_ada_backward_cuda(int aligned_height, int aligned_width, float spatial_scale,
-                        at::Tensor& top_grad, at::Tensor& rois, at::Tensor& bottom_grad)
-{
-    // Number of ROIs
-    int num_rois = rois.size(0);
-    int size_rois = rois.size(1);
-    if (size_rois != 5)
-    {
-        return 0;
-    }
+int roi_align_ada_backward_cuda(at::Tensor top_grad, at::Tensor rois,
+                            int pooled_height, int pooled_width,
+                            float spatial_scale, int sample_num,
+                            at::Tensor bottom_grad) {
+  CHECK_INPUT(top_grad);
+  CHECK_INPUT(rois);
+  CHECK_INPUT(bottom_grad);
 
-    // batch size
-    int batch_size = bottom_grad.size(0);
-    //if (batch_size != 1)
-    //{
-     //   return 0;
-    //}
-    // data height
-    int data_height = bottom_grad.size(2);
-    // data width
-    int data_width = bottom_grad.size(3);
-    // Number of channels
-    int num_channels = bottom_grad.size(1);
+  // Number of ROIs
+  int num_rois = rois.size(0);
+  int size_rois = rois.size(1);
+  if (size_rois != 5) {
+    printf("wrong roi size\n");
+    return 0;
+  }
 
-    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-    ROIAlignAdaBackwardLaucher(
-        top_grad.contiguous().data<float>(), spatial_scale, batch_size, num_rois, data_height,
-        data_width, num_channels, aligned_height,
-        aligned_width, rois.contiguous().data<float>(),
-        bottom_grad.contiguous().data<float>(), stream);
+  int num_channels = bottom_grad.size(1);
+  int data_height = bottom_grad.size(2);
+  int data_width = bottom_grad.size(3);
 
-    return 1;
+  ROIAlignAdaBackwardLaucher(top_grad, rois, spatial_scale, sample_num,
+                          num_channels, data_height, data_width, num_rois,
+                          pooled_height, pooled_width, bottom_grad);
+
+  return 1;
 }
 
-
-extern "C" int roi_align_dense_ada_forward_cuda(int aligned_height, int aligned_width, float spatial_scale,
-                        at::Tensor& features, at::Tensor& rois, at::Tensor& output)
-{
-    // Number of ROIs
-    int num_rois = rois.size(0);
-    int size_rois = rois.size(1);
-    if (size_rois != 5)
-    {
-        return 0;
-    }
-    // batch size
-    //int batch_size = THCudaTensor_size(state, features, 0);
-    //if (batch_size != 1)
-    //{
-    //    return 0;
-    //}
-
-
-    // data height
-    int data_height = features.size(2);
-    // data width
-    int data_width = features.size(3);
-    // Number of channels
-    int num_channels = features.size(1);
-
-    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-
-    ROIAlignDenseAdaForwardLaucher(
-        features.contiguous().data<float>(), spatial_scale, num_rois, data_height,
-        data_width, num_channels, aligned_height,
-        aligned_width, rois.contiguous().data<float>(),
-        output.contiguous().data<float>(), stream);
-
-    return 1;
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("forward", &roi_align_forward_cuda, "Roi_Align forward (CUDA)");
+  m.def("backward", &roi_align_backward_cuda, "Roi_Align backward (CUDA)");
+  m.def("ada_forward", &roi_align_ada_forward_cuda, "Roi_Align_ada forward (CUDA)");
+  m.def("ada_backward", &roi_align_ada_backward_cuda, "Roi_Align_ada backward (CUDA)");
 }
-
-extern "C" int roi_align_dense_ada_backward_cuda(int aligned_height, int aligned_width, float spatial_scale,
-                        at::Tensor& top_grad, at::Tensor& rois, at::Tensor& bottom_grad)
-{
-    // Grab the input tensor
-    /*float * top_grad_flat = THCudaTensor_data(state, top_grad);
-    float * rois_flat = THCudaTensor_data(state, rois);
-
-    float * bottom_grad_flat = THCudaTensor_data(state, bottom_grad);
-
-    // Number of ROIs
-    int num_rois = THCudaTensor_size(state, rois, 0);
-    int size_rois = THCudaTensor_size(state, rois, 1);
-    if (size_rois != 5)
-    {
-        return 0;
-    }
-
-    // batch size
-    int batch_size = THCudaTensor_size(state, bottom_grad, 0);
-    //if (batch_size != 1)
-    //{
-     //   return 0;
-    //}
-    // data height
-    int data_height = THCudaTensor_size(state, bottom_grad, 2);
-    // data width
-    int data_width = THCudaTensor_size(state, bottom_grad, 3);
-    // Number of channels
-    int num_channels = THCudaTensor_size(state, bottom_grad, 1);
-
-    cudaStream_t stream = THCState_getCurrentStream(state);*/
-    // Number of ROIs
-    int num_rois = rois.size(0);
-    int size_rois = rois.size(1);
-    if (size_rois != 5)
-    {
-        return 0;
-    }
-
-    // batch size
-    int batch_size = bottom_grad.size(0);
-    //if (batch_size != 1)
-    //{
-     //   return 0;
-    //}
-    // data height
-    int data_height = bottom_grad.size(2);
-    // data width
-    int data_width = bottom_grad.size(3);
-    // Number of channels
-    int num_channels = bottom_grad.size(1);
-
-    cudaStream_t stream = at::cuda::getCurrentCUDAStream();
-    ROIAlignDenseAdaBackwardLaucher(
-        top_grad.contiguous().data<float>(), spatial_scale, batch_size, num_rois, data_height,
-        data_width, num_channels, aligned_height,
-        aligned_width, rois.contiguous().data<float>(),
-        bottom_grad.contiguous().data<float>(), stream);
-
-    return 1;
-}
-
-/*PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("roi_align_forward_cuda", &roi_align_forward_cuda, "ROIAlign_forward");
-  m.def("roi_align_backward_cuda", &roi_align_backward_cuda, "ROIAlign_backward");
-  m.def("roi_align_ada_forward_cuda", &roi_align_ada_forward_cuda, "ROIAlign_Ada_forward");
-  m.def("roi_align_ada_backward_cuda", &roi_align_ada_backward_cuda, "ROIAlign_Ada_backward");
-  m.def("roi_align_dense_ada_forward_cuda", &roi_align_dense_ada_forward_cuda, "ROIAlign_Dense_Ada_forward");
-  m.def("roi_align_dense_ada_backward_cuda", &roi_align_dense_ada_backward_cuda, "ROIAlign_Dense_Ada_backward");
-}*/
